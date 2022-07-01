66d12c68802b38687bf77675ba27077739cecad5
Ritu Gothwal <gothwalritu@gmail.com>
2022-07-01 13:47:38 -0700

    the final code for the election analysis

--- PyPoll.py
+++ PyPoll.py
@@ -84,56 +84,93 @@ with open(file_to_load) as election_data:
        candidate_votes[candidate_name] +=1
 
 
+with open(file_to_save, "w") as txt_file:
+
+# Print the final vote count to the terminal.
+    election_results = (
+
+        f"\nElection Results\n"
+
+        f"-------------------------\n"
+
+        f"Total Votes: {total_votes:,}\n"
+
+        f"-------------------------\n")
+
+    print(election_results, end="")
+
+    # Save the final vote count to the text file.
+
+    txt_file.write(election_results)
 
 
-# Iterate through the candidate list.
+    # Iterate through the candidate list.
 
-for candidate_name in candidate_votes:
+    for candidate_name in candidate_votes:
 
-    # Retrieve vote count of a candidate.
+        # Retrieve vote count of a candidate.
 
-    votes = candidate_votes[candidate_name]
+        votes = candidate_votes[candidate_name]
 
-    # Calculate the pec=rcentage of votes.
+        # Calculate the pec=rcentage of votes.
 
-    vote_percentage = float(votes) / float(total_votes)* 100
+        vote_percentage = float(votes) / float(total_votes)* 100
 
-    # Print the candidate name and percentage of votes.
+        # Print the candidate name and percentage of votes.
 
-    print(f"{candidate_name}: recieved {vote_percentage:.1f}% of the vote.")
+        # print(f"{candidate_name}: recieved {vote_percentage:.1f}% of the vote.")
 
-
-    # Winning candidate and winning count tracker
-
-
-    # Determining winning vote count and candidate
-
-    # 1. Determining if the votes are greater than the winning count.
-
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-
-
-        winning_count = votes
-
-        winning_percentage = vote_percentage
-
-        winning_candidate = candidate_name
     
 
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        # Print each candidate, their voter count, and percentage to the terminal.
+
+        print(candidate_results)
+
+        #  Save the candidate results to our text file.
+
+        txt_file.write(candidate_results)
 
 
-winning_candidate_summary = (
+        # Winning candidate and winning count tracker
 
-    f"----------------------------------------------------------------------\n"
 
-    f"Winner: {winning_candidate}\n"
+        # Determining winning vote count and candidate
 
-    f"Winning Vote Count: {winning_count}\n"
+        # 1. Determining if the votes are greater than the winning count.
 
-    f"Winning percentage: {winning_percentage:.1f}%\n"
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
 
-    f"-----------------------------------------------------------------------\n"
 
-)
+            winning_count = votes
 
-print(winning_candidate_summary)
+            winning_percentage = vote_percentage
+
+            winning_candidate = candidate_name
+        
+
+
+
+    winning_candidate_summary = (
+
+        f"----------------------------------------------------------------------\n"
+
+        f"Winner: {winning_candidate}\n"
+
+        f"Winning Vote Count: {winning_count}\n"
+
+        f"Winning percentage: {winning_percentage:.1f}%\n"
+
+        f"-----------------------------------------------------------------------\n"
+
+    )
+
+    print(winning_candidate_summary)
+
+      # Save the winning candidate's results to the text file.
+
+    txt_file.write(winning_candidate_summary)
+
+
+
